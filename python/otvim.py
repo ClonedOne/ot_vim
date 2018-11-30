171943d70d277e6130a0783c0f8181763d76e110
ClonedOne <severi.giorgio@gmail.com>
2018-11-30 03:28:57 -0500

    unifying the diff cases

--- python/otvim.py
+++ python/otvim.py
@@ -1,4 +1,5 @@
 from enum import Enum
+from pprint import pprint
 import vim
 
 class OperationType(Enum):
@@ -13,7 +14,7 @@ class OTVim:
 
 
     def start(self):
-        self._buffer = self._vim.current.buffer
+        self._buffer = self._vim.current.buffer[:]
         self._cursor_pos = self._vim.current.window.cursor
 
         #  set up event listeneres
@@ -26,54 +27,30 @@ class OTVim:
 
     def check_buffer(self):
         #  print("Check buffer test")
-        print(list(self._vim.current.window.cursor))
-        #  print(len(self._vim.current.buffer))
+        print((self._vim.current.window.cursor))
+        #  pprint([i for i in self._vim.current.buffer])
+
+        new_buffer = self._vim.current.buffer[:]
+        new_cursor = self._vim.current.window.cursor
+
+        diff = find_difs(self._buffer, new_buffer, new_cursor)
+
+        self._buffer = new_buffer
+        self._cursor_pos = new_cursor
+
+        #  print(new_cursor)
 
 
 
-def find_difs(b1, b2):
+def find_difs(b1, b2, cursor):
     """ Finds the position of the change in the buffer and returns the change.
 
-    Returns a tuple (X (ROW), Y (COLUMN), CHAR, OPERATION).
-
-    Vim beffer change event only return the new position of the cursor and the
-    content of the entire buffer as a 2d array.  This method will identify the
-    changed characters and the operation type.  The function should (hopefully)
-    run in O(longest line modified * number of lines inserted/deleted).  Due to
-    the limited information provided by Vim we will have to dinstinguish
-    between different situations.
-
-
-    LINE INSERT OPERATIONS:
-
-    If it is an insert of a newline then the length of the new line will be 1
-    and the first character will be ''.
-
-    A line pasted will still have the cursor in position 0 but will have length
-    > 0.
-
-
-    LINE DELETE OPERATIONS:
-
-    If a single char delete operation removed the newline character, the
-    cursor_x will be equal to the length of the line above in the old buffer.
-    The length of the above line in the new buffer will also be longer than
-    what it was before.
-
-    If a delete operation removed an entire line, cursor_x will be at position
-    0 on the line above.  The length of the above line in the new buffer will
-    not be changed
-
-
-    IN-LINE INSERT/DELETE OPERATIONS:
-
-    Transform the corresponding line of both (old and new) buffers into
-    position-preserving sets and perfor set subraction to find differences.
-
-    - old_buffer \ new_buffer in case of delete
-    - new_buffer \ old_buffer in case of insert
+    Returns a list of tuples (1D-POS, CHAR, OPERATION).
 
     """
+
+    print (len(b1), b1)
+    print (len(b2), b2)
 
     cursor_y = cursor[0] - 1
     cursor_x = cursor[1]
@@ -82,7 +59,7 @@ def find_difs(b1, b2):
     print('lines delta:', lines_delta)
     diff = []
 
-    affected_lines = range(cursor_y, cursor_y + 1 + abs(lines_delta))
+    affected_lines = range(cursor_y - 1, cursor_y + 1 + abs(lines_delta))
     print('Affected lines:', affected_lines)
 
     for line in affected_lines:
@@ -91,32 +68,35 @@ def find_difs(b1, b2):
         s2 = set()
 
         if (line < len(b1)):
-            s1 = set(
-                [
-                    (line, pos, b1[line][pos])
-                    if b1[line][pos] != ''
-                    else (line, pos, '\n')
-                    for pos in range(len(b1[line]))
-                ]
-            )
+            print('b1 line:', b1[line])
+            s1 = set([(line, pos, b1[line][pos]) for pos in range(len(b1[line]))])
+            if (line + 1 < len(b1)):
+                s1.add((line, len(b1[line]), '\n'))
 
         if (line < len(b2)):
-            s2 = set(
-                [
-                    (line, pos, b2[line][pos])
-                    if b2[line][pos] != ''
-                    else (line, pos, '\n')
-                    for pos in range(len(b2[line]))
-                ]
-            )
+            print('b2 line:', b2[line])
+            s2 = set([(line, pos, b2[line][pos]) for pos in range(len(b2[line]))])
+            if (line + 1 < len(b2)):
+                s2.add((line, len(b2[line]), '\n'))
+
+        print('s1:', s1)
+        print('s2:', s2)
 
         if (s1 == s2):
            continue
 
         else:
-            diff_set = s1 - s2
+            sub_set = s1 - s2
             add_set = s2 - s1
+            print('sub set:', sub_set)
+            print('add set:', add_set)
 
-        print('diff set:', diff_set)
-        print('add set:', add_set)
+        for r in sub_set:
+            pos_1d = sum([len(b1[line]) for line in range(r[0])]) + r[1]
+            diff.append((pos_1d, r[2], OperationType.DELETE))
 
+        for i in add_set:
+            pos_1d = sum([len(b2[line]) for line in range(i[0])]) + i[1]
+            diff.append((pos_1d, i[2], OperationType.INSERT))
+
+    return diff
